405eb95f84c16e4cae395e1fc3f2acf956723443
xsf <lambor_xs@163.com>
2025-08-05 20:06:47 +0800

    "feat:add_user_management_features"

---
+++ src/main/java/com/spring/aicodemother/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.spring.aicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.spring.aicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
