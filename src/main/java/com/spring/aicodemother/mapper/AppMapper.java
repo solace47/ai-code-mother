a0fe7e0293af333655d2b75c1eb2acc2c0b4875d
lambor <lambor_xs@163.com>
2025-08-11 22:48:41 +0800

    feat(app): 新增应用(App)模块及相关API

---
+++ src/main/java/com/spring/aicodemother/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.spring.aicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.spring.aicodemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author lambor
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
