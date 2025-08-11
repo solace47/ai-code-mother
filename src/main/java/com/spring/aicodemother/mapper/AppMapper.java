8883b30a4c8180db445ff1d23cf5afc50912f92f
xsf <lambor_xs@163.com>
2025-08-11 22:48:41 +0800

    feat(app): 新增应用(App)模块及相关API

---
+++ src/main/java/com/spring/aicodemother/mapper/AppMapper.java
@@ -0,0 +1,12 @@
+package com.spring.aicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.spring.aicodemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
