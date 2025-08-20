8f6d05f40f68374fe0f7b9b3d7bf5fb9a1a2c251
xsf <lambor_xs@163.com>
2025-08-20 20:14:07 +0800

    feat: 完善AI代码生成平台功能 - 添加Redis聊天记忆存储配置 - 优化聊天历史服务 - 更新应用配置和静态资源控制器

--- src/main/java/com/spring/aicodemother/AiCodeMotherApplication.java
+++ src/main/java/com/spring/aicodemother/AiCodeMotherApplication.java
@@ -1,5 +1,6 @@
 package com.spring.aicodemother;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 应用程序启动类
  * 
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisEmbeddingStoreAutoConfiguration.class)
 @MapperScan("com.spring.aicodemother.mapper") // 告诉Spring Boot自动扫描并注册指定包路径下的MyBatis Mapper接口
 public class AiCodeMotherApplication {
 
