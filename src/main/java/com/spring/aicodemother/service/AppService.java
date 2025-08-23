eaaa131e17142afd4fba01736fd2fbc80dd227b4
lambor <lambor_xs@163.com>
2025-08-24 00:02:37 +0800

    feat: 添加网页截图功能 - 集成腾讯云COS存储 - 使用环境变量配置敏感信息

--- src/main/java/com/spring/aicodemother/service/AppService.java
+++ src/main/java/com/spring/aicodemother/service/AppService.java
@@ -17,6 +17,8 @@ import java.util.List;
  * @author lambor
  */
 public interface AppService extends IService<App> {
+    void generateAppScreenshotAsync(Long appId, String appUrl);
+
     AppVO getAppVO(App app);
 
     QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
